167bcb6bf750571d46dae953f2b788ba0bc4f358
astrobeard <astrobeard21@gmail.com>
2019-05-10 17:59:35 -0400

    snapshot

--- halos/src/snapshot.h
+++ halos/src/snapshot.h
@@ -5,16 +5,16 @@
 #ifndef SNAPSHOT_H 
 #define SNAPSHOT_H 
 
-#define max(a, b) ({ 
-	__typeof__ (a) _a = (a); 
-	__typeof__ (b) _b = (b); 
-	_a > _b ? _a : _b; 
+#define max(a, b) ({ \
+	__typeof__ (a) _a = (a); \
+	__typeof__ (b) _b = (b); \
+	_a > _b ? _a : _b; \
 }) 
 
-#define min(a, b) ({
-	__typeof__ (a) _a = (a); 
-	__typeof__ (b) _b = (b); 
-	_a < _b ? _a : _b; 	
+#define min(a, b) ({ \
+	__typeof__ (a) _a = (a); \
+	__typeof__ (b) _b = (b); \
+	_a < _b ? _a : _b; \
 }) 
 
 /* 
