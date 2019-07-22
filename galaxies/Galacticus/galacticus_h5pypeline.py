d66fdb07c491aa28dfb22f9b5177be1dc15f483c
astrobeard <astrobeard21@gmail.com>
2019-07-22 14:03:22 -0400

    h5pypeline

--- galaxies/Galacticus/galacticus_h5pypeline.py
+++ galaxies/Galacticus/galacticus_h5pypeline.py
@@ -71,57 +71,33 @@ def write_concentration(indata, outdata):
 					indata["%s/darkMatterProfileScale" % (stem)][:])) 
 			) 
 
+def write_IDs(indata, outdata): 
+	"""
+	Writes satellite IDs to the output file. The IDs are strings of the format: 
+
+	mergerTreeIndex.nodeIndex 
+
+	These are both subgroups stored at "Outputs/OutputX/nodeData" in raw 
+	Galacticus outputs 
+	""" 
+	for i in range(len(_OUTPUT_Z_)): 
+		stem = "Outputs/Output%d/nodeData" % (i + 1) 
+		IDs = len(indata["%s/nodeIndex" % (stem)]) * [None] 
+		for j in range(len(IDs)): 
+			IDs[j] = "%d.%d" % (
+				indata["%s/mergerTreeIndex"][j], 
+				indata["%s/nodeIndex"][j]
+			) 
+		outdata["%s/satelliteID" % (get_z_string(_OUTPUT_Z_[i]))] = np.array(IDs)  
+
 
 if __name__ == "__main__": 
 	# open the Galacticus output and the condensed hdf5 output file
 	indata = h5py.File(sys.argv[1], 'r')  
 	with h5py.File(sys.argv[2], 'w') as outdata: 
-		
 		write_phase_space(indata, outdata) 
 		write_mass(indata, outdata) 
 		write_concentration(indata, outdata) 
-
-		# # for each output redshift 
-		# for i in range(len(_OUTPUT_Z_)): 
-		# 	# Store the redshift in a z0p00 format where p represents the decimal 
-		# 	z_str = get_z_string(_OUTPUT_Z_[i]) 
-		# 	# Satellite phase space info 
-		# 	phase_space = tuple(["satellitePositionX", "satellitePositionY", 
-		# 		"satellitePositionZ", "satelliteVelocityX", "satelliteVelocityY", 
-		# 		"satelliteVelocityZ"]) 
-		# 	for j in phase_space: 
-		# 		"""
-		# 		Store this phase space coordinate at this output time 
-
-		# 		example of where this is found in raw Galacticus output: 
-		# 			indata["Outputs/Output1/nodeData/satellitePositionX"]
-		# 		"""
-		# 		indata_key = "Outputs/Output%d/nodeData/%s" % (i + 1, j) 
-		# 		outdata["%s/%s" % (z_str, j)] = indata[indata_key][:]
-
-		# 	"""
-		# 	Dwarf Structural Parameters 
-		# 	=========================== 
-		# 	Mass :: 
-		# 		Stored under nodeData/basicMass 
-		# 		Write this out for each satellite at each timestep as well 
-
-		# 	Concentration :: 
-		# 		Rvir / Rscale 
-		# 		Write this at each output timestep to the output file 
-		# 	""" 
-		# 	# The mass 
-		# 	indata_key = "Outputs/Output%d/nodeData/basicMass" % (i + 1)
-		# 	outdata["%s/satelliteMass" % (z_str)] = indata[indata_key][:] 
-
-		# 	# The concentration 
-		# 	outdata["%s/satelliteConcentration" % (z_str)] = np.array(
-		# 		map(lambda x, y: x / y, 
-		# 			indata["Outputs/Output%d/nodeData/nodeVirialRadius" % (
-		# 				i + 1)][:], 
-		# 			indata["Outputs/Output%d/nodeData/darkMatterProfileScale" % (
-		# 				i + 1)][:]
-		# 			)) 
 
 
 
